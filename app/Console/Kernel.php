cf290161282f5ed93bfbd734fc6d118be9563e0d
AidarIlyasov <06moboru@mail.ru>
2022-08-21 19:35:50 +0300

    initial commit

---
+++ app/Console/Kernel.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Console;
+
+use App\Models\User;
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+
+class Kernel extends ConsoleKernel
+{
+    /**
+     * Define the application's command schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @return void
+     */
+    protected function schedule(Schedule $schedule)
+    {
+        // $schedule->command('inspire')->hourly();
+        $schedule->command('model:prune', [
+            '--except' => [User::class], // исключить prune определенных моделей при prune всех других обнаруженных моделей
+        ])->daily();
+    }
+
+    /**
+     * Register the commands for the application.
+     *
+     * @return void
+     */
+    protected function commands()
+    {
+        $this->load(__DIR__.'/Commands');
+
+        require base_path('routes/console.php');
+    }
+}
